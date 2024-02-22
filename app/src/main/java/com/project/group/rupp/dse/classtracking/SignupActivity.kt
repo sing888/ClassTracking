8dfd261bbd64148098abb731a7fca131ecb8d576
sing888 <singdoung168@gmail.com>
2024-02-22 10:37:08 +0700

    ADD Signin/signup

---
+++ app/src/main/java/com/project/group/rupp/dse/classtracking/SignupActivity.kt
@@ -0,0 +1,26 @@
+package com.project.group.rupp.dse.classtracking
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import com.project.group.rupp.dse.classtracking.databinding.ActivitySignUpBinding
+
+class SignupActivity: Activity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signUpButton.setOnClickListener{
+            // start main activity
+            startActivity(Intent(this, MainActivity::class.java)
+                .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK))
+        }
+    }
+
+
+}
