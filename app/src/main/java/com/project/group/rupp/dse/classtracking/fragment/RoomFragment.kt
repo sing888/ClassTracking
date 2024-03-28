436120f813c7d93a1d1db6020ab833f6d107c8bd
sing888 <singdoung168@gmail.com>
2024-03-28 14:31:09 +0700

    Update Room page and connect some code

--- app/src/main/java/com/project/group/rupp/dse/classtracking/fragment/RoomFragment.kt
+++ app/src/main/java/com/project/group/rupp/dse/classtracking/fragment/RoomFragment.kt
@@ -1,13 +1,18 @@
 package com.project.group.rupp.dse.classtracking.fragment
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.project.group.rupp.dse.classtracking.R
+import com.project.group.rupp.dse.classtracking.databinding.FragmentRoomBinding
 
 class RoomFragment : Fragment() {
+    private var _binding: FragmentRoomBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,5 +20,20 @@ class RoomFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_room, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentRoomBinding.bind(view)
+
+        binding.btnRoomStudent.setOnClickListener {
+            binding.btnRoomStudent.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFD1E8D6")));
+            binding.btnRoomTeacher.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFFFFFFF")));
+        }
+
+        binding.btnRoomTeacher.setOnClickListener {
+            binding.btnRoomTeacher.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFD1E8D6")));
+            binding.btnRoomStudent.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFFFFFFF")));
+        }
     }
 }
